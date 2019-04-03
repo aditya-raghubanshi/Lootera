b2f6210b1531fe29b8c7c5dcf680df1814bc3094
Pankti Shah <pashah@usc-guestwireless-upc-new-000140.usc.edu>
2019-04-03 10:13:09 -0700

    Final MainMenu: MainToon

---
+++ Lootera/Assets/quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    public void doQuit()
+    {
+        Debug.Log("Has QUIT Game");
+        Application.Quit(1);
+    }
+}
