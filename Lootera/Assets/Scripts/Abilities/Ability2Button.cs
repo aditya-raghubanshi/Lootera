0fbc111500a4dac69ebf401cbb8fe4bc431e5777
aditya-raghubanshi <aditya.raghuabanshi@gmail.com>
2019-04-03 09:11:28 -0700

    DemoApril3
    Added a few abilities

--- Lootera/Assets/Scripts/Abilities/Ability2Button.cs
+++ Lootera/Assets/Scripts/Abilities/Ability2Button.cs
@@ -9,6 +9,7 @@ public class Ability2Button : MonoBehaviour
     AbilitySlots slots;
     int ability2;
     public Sprite healingShout;
+    public Sprite shieldBash;
     Button button;
     void Start()
     {
@@ -21,6 +22,9 @@ public class Ability2Button : MonoBehaviour
         {
             case 1:
                 button.GetComponent<Image>().sprite = healingShout;
+                break;
+            case 2:
+                button.GetComponent<Image>().sprite = shieldBash;
                 break;
         }
         // Add more cases as we add new Abilities
