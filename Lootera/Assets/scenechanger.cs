3fca212af0fe6ccfff4dae4f8fcd3e2bbedd6c71
Pankti Shah <pashah@Panktis-MacBook-Pro.local>
2019-04-10 20:24:54 -0700

    Final UpdatedMainMenu:MainMenu

---
+++ Lootera/Assets/scenechanger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechanger: MonoBehaviour
+{
+    public void changeToScene(int changeToScene)
+    {
+        SceneManager.LoadScene(changeToScene);
+    }
+}
