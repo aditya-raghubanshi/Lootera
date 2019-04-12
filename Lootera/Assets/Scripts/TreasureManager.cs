a49d250b4ae52d91fc1f102a74c5fde8ae59c2d8
sriaz08 <36119785+sriaz08@users.noreply.github.com>
2019-04-12 07:29:32 -0700

    Added Master Inventory

--- Lootera/Assets/Scripts/TreasureManager.cs
+++ Lootera/Assets/Scripts/TreasureManager.cs
@@ -4,50 +4,58 @@ using UnityEngine;
 using UnityEngine.AI;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System;
 
 public class TreasureManager : MonoBehaviour
 {
-    [SerializeField] public GameObject[] weapons;                // The weapon prefab to be spawned.
-    [SerializeField] public GameObject[] weaponObjects;                // The weapon objects spawned.
+    [SerializeField] public GameObject treasureChest = null;
+    int[] weapons = new int[] {36, 36, 37, 37};                // 36 viking sword, 37 dragonblade
+    //GameObject[] weaponObjects;                // The weapon objects spawned.
     private Vector3[] weaponPositions;
-    [SerializeField] public float spawnTime = 0f;            // How long between each spawn.
-    [SerializeField] public float SpawnRadius = 1000;
-    [SerializeField] public int spawnNumber = 15;
+    [SerializeField] public float spawnTime = 4f;            // How long between each spawn.
+    [SerializeField] public float SpawnRadius = 5000;
+    [SerializeField] public int spawnNumber = 4;
     static int weaponIndex = 0;
+    public PlayerInventory playerInventory;
 
-    public static List<WeaponSaveData> inventory = new List<WeaponSaveData>();
+    public static List<int> inventory = new List<int>();
 
+    protected InteractionButton interaction;
     protected GameObject player;
     //public Transform interactionTransform;
 
     void Start()
     {
         LoadSerializedWeapons();
+        interaction = FindObjectOfType<InteractionButton>();
         player = GameObject.Find("Player");
         //interactionTransform = this.transform;
 
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         weaponPositions = new Vector3[weapons.Length];
-        weaponObjects = new GameObject[weapons.Length];
+        //weaponObjects = new GameObject[weapons.Length];
         for (int i = 0; i < spawnNumber; i++)
         {
             Spawn();
         }
-
     }
 
-    public void PickTreasure()
+    void Update()
     {
-        Debug.Log("INTERACTION PRESSED!");
-        for (int i = 0; i < weaponPositions.Length; i++)
+        if (interaction.Pressed)
         {
 
-            Debug.Log("LOOPING....");
-            float distance = Vector3.Distance(player.transform.transform.position, weaponPositions[i]);
-            if (distance <= 3f)
+            Debug.Log("INTERACTION PRESSED!");
+            for (int i = 0; i < weaponPositions.Length; i++)
             {
-                Interact(i);
-                break;
+
+                Debug.Log("LOOPING....");
+                float distance = Vector3.Distance(player.transform.transform.position, weaponPositions[i]);
+                if (distance <= 3f)
+                {
+                    Interact(i);
+                    break;
+                }
             }
         }
     }
@@ -61,8 +69,12 @@ public class TreasureManager : MonoBehaviour
     public void Interact(int weaponIndex)
     {
         Debug.Log("Interacting with Treausre");
-        inventory.Add(new WeaponSaveData(weapons[weaponIndex].ToString()));
-        Destroy(weaponObjects[weaponIndex]);
+        //inventory.Add(new WeaponSaveData(weapons[weaponIndex].ToString()));
+        //Destroy(weaponObjects[weaponIndex]);
+        inventory.Add(weapons[weaponIndex]);
+        playerInventory.addItemToMainInventory(weapons[weaponIndex], 1);
+        playerInventory.openMainInventory();
+        playerInventory.openCharacterInventory();
 
         SaveSerializedWeapons();
     }
@@ -77,7 +89,8 @@ public class TreasureManager : MonoBehaviour
         // Create an instance of the weapon prefab at the randomly selected spawn point's position and rotation.
         Debug.Log("index:" + weaponIndex);
         weaponPositions[weaponIndex] = RandomNavmeshLocation(SpawnRadius);
-        weaponObjects[weaponIndex] = Instantiate(weapons[weaponIndex], weaponPositions[weaponIndex], rot);
+        //weaponObjects[weaponIndex] = Instantiate(weapons[weaponIndex], weaponPositions[weaponIndex], rot);
+        Instantiate(treasureChest, weaponPositions[weaponIndex], rot);
         weaponIndex++;
     }
 
@@ -85,14 +98,14 @@ public class TreasureManager : MonoBehaviour
     {
         while (true)
         {
-            Vector3 randomDirection = Random.insideUnitSphere * radius * (weaponIndex + 2);
+            Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * radius * (weaponIndex + 2);
             //randomDirection += transform.position;
             NavMeshHit hit;
             Vector3 finalPosition = Vector3.zero;
             if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
             {
                 finalPosition = hit.position;
-                finalPosition.y = 2;
+                finalPosition.y = 1;
                 return finalPosition;
             }
         }
@@ -120,7 +133,8 @@ public class TreasureManager : MonoBehaviour
             FileStream fileStream = File.Open(serializedWeaponFileName, FileMode.Open);
             if (fileStream.Length != 0)
             {
-                inventory = (List<WeaponSaveData>)bf.Deserialize(fileStream);
+                //inventory = (List<WeaponSaveData>)bf.Deserialize(fileStream);
+                inventory = (List<int>)bf.Deserialize(fileStream);
             }
 
             fileStream.Close();
@@ -131,7 +145,7 @@ public class TreasureManager : MonoBehaviour
         }
     }
 
-    [System.Serializable]
+    [Serializable]
     public struct WeaponSaveData
     {
         public string name;
