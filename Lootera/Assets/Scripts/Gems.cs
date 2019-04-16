4f73d40d16c500607f9c9e4104cd1f4b6b39e58c
Pankti Shah <pashah@panktis-mbp.home>
2019-04-16 15:57:04 -0700

    Gems/Level/RandomMaps

---
+++ Lootera/Assets/Scripts/Gems.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Gems : MonoBehaviour
+{
+    public static int gems;
+
+
+    public void SetGems(int i)
+    {
+        gems = i;
+    }
+
+    public int GetGems()
+    {
+        return gems;
+    }
+
+}
---
+++ Lootera/Assets/Scripts/LevelText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text text1;
+   Level level;
+    void Start()
+    {
+        level = FindObjectOfType<Level>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(gems.GetGems());
+        text1.text = level.GetLevel().ToString("0");
+    }
+}
