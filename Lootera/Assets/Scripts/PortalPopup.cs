2bcb468930a93a04a2c29b6b527036c66ee1a108
sachinh2503 <36029196+sachinh2503@users.noreply.github.com>
2019-04-17 01:22:30 -0700

    Added Enemy manager, minotaur and demons to Second map

--- Lootera/Assets/Scripts/PortalPopup.cs
+++ Lootera/Assets/Scripts/PortalPopup.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PortalPopup : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject popup;
-	void Start(){
+    GameObject dungeonUI,popup;
+	/*void Start(){
 		popup = GameObject.Find("ExitPopup");
-		popup.SetActive(false);
-	}
+		//popup.SetActive(false);
+	}*/
 	public void OnTriggerEnter(Collider col){
 		Debug.Log("..................................................................................Collided portal");
+		dungeonUI = GameObject.Find("Dungeon UI");
+		popup = dungeonUI.transform.Find("ExitPopup").gameObject;
 		string nameOfOther = col.gameObject.name;
         if(nameOfOther.Equals("Player"))
         {
