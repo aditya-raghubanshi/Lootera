71885968bd0cd58b16227561edc1c5fc8c2b42ae
Aakash Dhongade <dhongadeaakash@gmail.com>
2019-04-16 20:38:02 -0700

    Added knight killing minatour

--- Lootera/Assets/Scripts/SwordAttack.cs
+++ Lootera/Assets/Scripts/SwordAttack.cs
@@ -8,18 +8,33 @@ public class SwordAttack : MonoBehaviour
     MeshCollider mesh;
     public float damage = 10f;
     Dungeon_Monster_Controller[] skeletons;
+    Minotaur_Controller[] minotaurs;
+
     void OnTriggerEnter(Collider col)
     {
         AttackButton button = FindObjectOfType<AttackButton>();
         //skeleton = FindObjectOfType<Dungeon_Monster_Controller>();
         skeletons = col.gameObject.GetComponents<Dungeon_Monster_Controller>();
+        minotaurs = col.gameObject.GetComponents<Minotaur_Controller>();
         if (skeletons != null)
         {
             if (button.pressed == true)
             {
-                foreach(Dungeon_Monster_Controller skeleton in skeletons)
+                foreach (Dungeon_Monster_Controller skeleton in skeletons)
                 {
-                    skeleton.Damage(10);
+                    skeleton.Damage(damage);
+                }
+            }
+        }
+        if (minotaurs != null)
+        {
+            if (button.pressed == true)
+            {
+                foreach (Minotaur_Controller minotaur in minotaurs)
+                {
+                    
+                        minotaur.Damage(damage);
+                    
                 }
             }
         }
